6dfddb598db5eb8b42b8eb3037cd75903f75041c
Just Gamer <lmaxl100ukr.net@gmail.com>
2024-09-20 22:42:10 +0300

    V.1

---
+++ Site Project/Controllers/PrivacyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site_Project.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
